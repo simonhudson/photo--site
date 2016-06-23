d191318c2083eda33a747024e0bdb878f96eb132
Simon Hudson <simon.hudson@experian.com>
2016-06-23 08:42:12 +0100

    Add intial structure

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <title>Photography Portfolio</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="css/styles.css" rel="stylesheet" />
+</head>
+<body>
+
+</body>
+</html>
